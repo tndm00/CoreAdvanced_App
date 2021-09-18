54f21496e8fa141012f614fae5e61a409d830dbd
bob <nhan.truong@interactivepartners.com.au>
2021-09-18 20:09:06 +0700

    Display role information

--- CoreAdvanced_App.Application/Implementation/RoleService.cs
+++ CoreAdvanced_App.Application/Implementation/RoleService.cs
@@ -139,7 +139,7 @@ namespace CoreAdvanced_App.Application.Implementation
 
         public async Task UpdateAsync(AppRoleViewModel roleVm)
         {
-            var role = await _roleManager.FindByIdAsync(roleVm.Id);
+            var role = await _roleManager.FindByIdAsync(roleVm.Id.ToString());
             role.Description = roleVm.Description;
             role.Name = roleVm.Name;
             await _roleManager.UpdateAsync(role);
