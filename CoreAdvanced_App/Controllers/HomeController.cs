5667e47f57712e0a9a028733a725800fc008204e
bob <nhan.truong@interactivepartners.com.au>
2021-10-18 23:27:14 +0700

    Install template and display on HomePage

--- CoreAdvanced_App/Controllers/HomeController.cs
+++ CoreAdvanced_App/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using CoreAdvanced_App.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CoreAdvanced_App.Controllers
 {
